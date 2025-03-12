9218376153daea19ee8b337e90b14602d7bfc09d
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-03-12 10:04:37 -0700

    Default quality profile ++

--- sonarqube-rust-sensor/src/main/java/com/jmcateer/sonarqube_rust_sensor/RustQualityProfile.java
+++ sonarqube-rust-sensor/src/main/java/com/jmcateer/sonarqube_rust_sensor/RustQualityProfile.java
@@ -1,25 +1,50 @@
 package com.jmcateer.sonarqube_rust_sensor;
 
 import java.util.List;
+import java.util.Objects;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
 
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONTokener;
+
 public class RustQualityProfile implements BuiltInQualityProfilesDefinition {
+
+    private static final Logger LOGGER = Loggers.get(RustQualityProfile.class);
+
 
     @Override
     public void define(Context context) {
         final NewBuiltInQualityProfile profile = context.createBuiltInQualityProfile("Rust rules", Constants.LANGUAGE_KEY)
                                                         .setDefault(true);
 
-        /*final List<SqlRules> rules = SQLDialectRules.INSTANCE.getGroupedRules();
+        //profile.activateRule("rust", "rule1");
+        InputStream inputStream = RustQualityProfile.class.getResourceAsStream("clippy-rules.json");
+        InputStreamReader streamReader = new InputStreamReader(inputStream, java.nio.charset.StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(streamReader);
 
-        for (SqlRules sqlRules : rules) {
-            for (Rule rule : sqlRules.getRule()) {
-                profile.activateRule(sqlRules.getRepoKey(), rule.getKey());
+        JSONTokener tokener = new JSONTokener(reader);
+        JSONArray json = new JSONArray(tokener);
+
+        LOGGER.warn("Read json={}", json);
+
+        for (int i=0; i<json.length(); i+=1) {
+            try {
+                JSONObject rule_o = json.getJSONObject(i);
+                String rule_id = rule_o.getString("rule_id");
+                profile.activateRule("rust", rule_id);
             }
-        }*/
-
-        // TODO read from cargo-clippy's list of rules
-        profile.activateRule("rust", "rule1");
+            catch (Throwable e) {
+              LOGGER.warn("Unexpected exception in RustQualityProfile::define reading json[{}] {}", i, e);
+            }
+        }
 
         profile.done();
     }
